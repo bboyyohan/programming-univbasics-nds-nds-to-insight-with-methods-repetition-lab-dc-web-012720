e0b956527ddbf385938c0551bc4799e853642045
Yo Park <yo.park93@yahoo.com>
2020-01-23 21:54:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -42,7 +42,7 @@ def total_gross(source)
   total = 0
 
   while i < dir_names.size do
-    dir_name = dir_names[i]
+    directory_list = dir_names[i]
     total += dir_to_earnings_hash[dir_name]
     i += 1
   end
